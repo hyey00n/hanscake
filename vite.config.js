3d153752dc23df45ffde7977b76de1248b44944a
정혜윤 <a20897479@mail.com>
2025-10-26 17:12:28 +0900

    4차 업로드 다시

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/hansapp/',
+  base: '/hans/',  // ← hansapp이 아니라 hans로!
 })
